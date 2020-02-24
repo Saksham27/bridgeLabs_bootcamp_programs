c68580559bc9822ee57a12cff9629f1f2d610901
Saksham Singh <54993459+Saksham27@users.noreply.github.com>
2020-02-24 14:13:58 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,32 @@
+
+// Power of a number 
+
+import java.util.*; 
+public class RaiseToPower {
+	
+	int raiseToPower(int num,int power) {
+		int result=1;
+		for(int i=1;i<=power;i++) {
+			result *= num; 
+		}
+		return result;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    int num,power;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		num = sc.nextInt();
+		System.out.println("Enter the power : ");
+		power = sc.nextInt();
+		
+		System.out.print("Result of " + num + " raised to the power of " + power + " : " + new RaiseToPower().raiseToPower(num,power));
+		
+		
+	}
+	
+}
+
+
