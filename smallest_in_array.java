5be175882987a7e593325ed7ded5386d33e2c548
Saksham Singh <54993459+Saksham27@users.noreply.github.com>
2020-02-25 12:16:30 +0530

    Create smallest_in_array.java

---
+++ smallest_in_array.java
@@ -0,0 +1,37 @@
+// Smallest element in an array 
+
+import java.util.*;
+public class Array {
+	
+	static int smallest(int[] arr,int length){
+		int counter=arr[0];
+		for(int i=1;i<length;i++){
+			if(arr[i]<counter){
+				counter=arr[i];
+			}
+		}
+		return counter;
+	}
+	
+	public static void main(String args[]){
+		int size;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter array length : ");
+		size = sc.nextInt();
+		if(size<1){
+			System.out.println("Invalid array size");
+		} else {
+			int[] a = new int[size];
+			
+			System.out.println("Enter Array items : ");
+			for(int i=0;i<size;i++){
+				a[i]=sc.nextInt();
+			}
+			
+			System.out.println("smallest no in array : "+ smallest(a,size));
+		}
+		
+	}
+}
